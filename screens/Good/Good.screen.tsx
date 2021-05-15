0a2eb83a7c61b75eb3f282ff9934b6badcbd0fb3
Olya <olga2000@gmail.com>
2021-05-15 12:43:18 +0700

    markup completed

--- screens/Good/Good.screen.tsx
+++ screens/Good/Good.screen.tsx
@@ -1,11 +1,184 @@
 import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { TRootStackParamList } from '../../types/Navigation.types';
-
+import arrowLeftImg from '../../assets/arrowLeftBlack.png';
+import good from '../../assets/good.png';
+import addBtn from '../../assets/add.png';
+import { statusBarHeight } from '../../constants/Layout.constant';
 interface IGoodScreenProps {
     navigation: StackNavigationProp<TRootStackParamList, 'Good'>;
 }
-export const GoodScreen: React.FC<IGoodScreenProps> = () => {
-    return <View></View>;
+export const GoodScreen: React.FC<IGoodScreenProps> = ({ navigation }) => {
+    return (
+        <View
+            style={{
+                backgroundColor: 'rgba(52, 187, 10, 0.2)',
+                flex: 1,
+                paddingTop: statusBarHeight + 24,
+                paddingHorizontal: 16,
+            }}
+        >
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            >
+                <Image source={arrowLeftImg} />
+            </TouchableOpacity>
+            <ScrollView style={{ paddingTop: 32 }}>
+                <Image
+                    source={good}
+                    style={{
+                        alignSelf: 'center',
+                    }}
+                />
+                <Text
+                    style={{
+                        fontSize: 18,
+                        fontWeight: '500',
+                        paddingTop: 24,
+                    }}
+                >
+                    Sandwich with cabbage
+                </Text>
+                <View style={{ paddingTop: 24, flexDirection: 'row' }}>
+                    <View
+                        style={{
+                            alignItems: 'center',
+                            marginRight: 16,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            Corbs
+                        </Text>
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            42g
+                        </Text>
+                    </View>
+                    <View
+                        style={{
+                            alignItems: 'center',
+                            marginRight: 16,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            Corbs
+                        </Text>
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            42g
+                        </Text>
+                    </View>
+                    <View
+                        style={{
+                            alignItems: 'center',
+                            marginRight: 16,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            Corbs
+                        </Text>
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            42g
+                        </Text>
+                    </View>
+                    <View
+                        style={{
+                            alignItems: 'center',
+                            marginRight: 16,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            Corbs
+                        </Text>
+                        <Text
+                            style={{
+                                fontSize: 14,
+                            }}
+                        >
+                            42g
+                        </Text>
+                    </View>
+                </View>
+                <Text
+                    style={{
+                        paddingTop: 24,
+                        fontSize: 16,
+                    }}
+                >
+                    Details
+                </Text>
+                <Text
+                    style={{
+                        paddingTop: 12,
+                        maxWidth: 200,
+                        fontSize: 12,
+                    }}
+                >
+                    Restaraunt and cafe which have this dish in your city
+                </Text>
+
+                <View
+                    style={{
+                        paddingTop: 16,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <View
+                        style={{
+                            borderWidth: 1,
+                            borderRadius: 16,
+                            width: 16,
+                            height: 16,
+                        }}
+                    ></View>
+                    <Text
+                        style={{
+                            paddingLeft: 8,
+                        }}
+                    >
+                        Percini
+                    </Text>
+                </View>
+            </ScrollView>
+            <TouchableOpacity>
+                <Image
+                    source={addBtn}
+                    style={{
+                        marginBottom: 24,
+                        alignSelf: 'center',
+                    }}
+                />
+            </TouchableOpacity>
+        </View>
+    );
 };
--- screens/Search/Search.screen.tsx
+++ screens/Search/Search.screen.tsx
@@ -1,11 +1,106 @@
 import React from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View } from 'react-native';
+import {
+    Image,
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from 'react-native';
 import { TRootStackParamList } from '../../types/Navigation.types';
-
+import { statusBarHeight } from '../../constants/Layout.constant';
+import Plus from '../../assets/plusWhite.png';
 interface ISearchScreenProps {
     navigation: StackNavigationProp<TRootStackParamList, 'Search'>;
 }
-export const SearchScreen: React.FC<ISearchScreenProps> = () => {
-    return <View></View>;
+export const SearchScreen: React.FC<ISearchScreenProps> = ({ navigation }) => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: '#34BB0A',
+                paddingHorizontal: 16,
+                paddingTop: statusBarHeight + 16,
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <Text
+                    style={{
+                        fontSize: 16,
+                        color: 'white',
+                        fontWeight: '500',
+                    }}
+                >
+                    Breakfast
+                </Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.goBack();
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 16,
+                            color: 'white',
+                            fontWeight: '500',
+                        }}
+                    >
+                        Cancel
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <TextInput
+                style={{
+                    height: 36,
+                    backgroundColor: 'white',
+                    borderRadius: 10,
+                    marginTop: 24,
+                    paddingHorizontal: 16,
+                }}
+                placeholder="Search food"
+            />
+            <ScrollView style={{ flex: 1, paddingTop: 8 }}>
+                <TouchableOpacity
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        paddingLeft: 48,
+                        paddingTop: 16,
+                        alignItems: 'center',
+                    }}
+                    onPress={() => {
+                        navigation.navigate('Good');
+                    }}
+                >
+                    <View>
+                        <Text
+                            style={{
+                                fontSize: 16,
+                                color: 'white',
+                                fontWeight: '500',
+                            }}
+                        >
+                            Tea with milk
+                        </Text>
+                        <Text
+                            style={{
+                                fontSize: 14,
+                                color: '#424242',
+                            }}
+                        >
+                            1 cup (240 ml) 23 kcal
+                        </Text>
+                    </View>
+                    <Image source={Plus} />
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    );
 };
